15bf536d4bfa2c52eefa29a236598348dba44e4f
t-town <tom.stappaerts@student.kuleuven.be>
2014-02-26 16:37:22 +0100

    Client/Serverpaar cursus

--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,36 @@
-import support.*;
-import client.*;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 
 public class Test {
 
-	public static void main(String[] args) {
-		String uri = "tweakers.net";
+	@SuppressWarnings("resource")
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		
+		String uri = "213.239.154.20";
 		int port = 80;
 		Socket clientSocket = new Socket(uri,port);
+		
 		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		System.out.println(clientSocket.toString());
 		
-		outToServer.writeBytes("GET / HTTP/1.0");
+		outToServer.writeBytes("GET / HTTP/1.1 \r\n");
+		System.out.println(clientSocket.toString());
+
+
+		String fromServer;
+		while((fromServer = inFromServer.readLine()) != null) {
+			System.out.println(fromServer);
+		}
+		
+		
 	}
 	
 }
--- src/client/Client.java
+++ src/client/Client.java
@@ -1,8 +1,31 @@
 package client;
 
 import support.Command;
-import java.net;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
 public class Client {
+	
+	
+	public static void main(String[] args) {
+		try {
+		BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
+		Socket clientSocket = new Socket("localhost", 6789); DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new
+		InputStreamReader(clientSocket.getInputStream()));
+		String sentence = inFromUser.readLine();
+		outToServer.writeBytes(sentence + '\n');
+		String modifiedSentence = inFromServer.readLine(); System.out.println("FROM SERVER: " + modifiedSentence); clientSocket.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		}
+	
+	
+	
+	
 	
 	/**
 	 * 
